25842408f662aa169e23bcbae789a0ee4356a9ef
ufoo68 <mitarou.yuutarou@gmail.com]>
2023-01-26 23:00:32 +0900

    グラフを実装

--- ui/index.js
+++ ui/index.js
@@ -3,12 +3,66 @@ const fetchVoteCount = async (deviceId) => {
   return res.json();
 }
 
+let dresses = [
+  {
+    id: 1,
+    label: '赤',
+    count: 0,
+    background: 'rgba(255, 99, 132, 0.2)',
+  },
+  {
+    id: 2,
+    label: '緑',
+    count: 0,
+    background: 'rgba(75, 192, 192, 0.2)',
+  },
+  {
+    id: 3,
+    label: '青',
+    count: 0,
+    background: 'rgba(54, 162, 235, 0.2)',
+  },
+]
 
-document.getElementById('refetch-button').addEventListener('click', () => {
-  Promise.all([1, 2, 3].map(async (deviceId) => {
-    const res = await fetchVoteCount(deviceId)
-    const count = res?.count ?? 0;
-    document.getElementById(`vote-${deviceId}`).innerHTML = count;
-  }));
+const ctx = document.getElementById('vote-chart');
+
+const chart = new Chart(ctx, {
+  type: 'bar',
+  data: {
+    labels: ['投票数'],
+    // labels: dresses.map((dress) => dress.label),
+    datasets: dresses.map(({ label, count, background }) => ({
+      label,
+      data: [count],
+      backgroundColor: background,
+    }))
+  },
+  options: {
+    scales: {
+      y: {
+        beginAtZero: true
+      }
+    }
+  }
 });
 
+const intervalId = setInterval(async () => {
+  dresses = await Promise.all(dresses.map(async (dress) => {
+    const res = await fetchVoteCount(dress.id)
+    const count = res?.count ?? 0;
+    return {
+      ...dress,
+      count,
+    }
+  }));
+  chart.data.datasets.forEach((dataset) => {
+    dataset.data = [dresses.find((dress) => dress.label === dataset.label).count];
+  });
+  chart.data.datasets = chart.data.datasets.sort((a, b) => b.data[0] - a.data[0]);
+  chart.update();
+}, 1000);
+
+document.getElementById('stop-button').addEventListener('click', () => {
+  console.info('stop update.');
+  clearInterval(intervalId);
+})

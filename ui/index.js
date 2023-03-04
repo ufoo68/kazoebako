dd698f86463b74fc64b6a744bfc08b57c23bf456
ufoo68 <mitarou.yuutarou@gmail.com]>
2023-03-04 14:35:04 +0900

    グラフを復活

--- ui/index.js
+++ ui/index.js
@@ -1,4 +1,4 @@
-const obnizId = '9759-0430';
+const obnizId = "9759-0430";
 const seonsors = [
   {
     id: 1,
@@ -17,6 +17,24 @@ const seonsors = [
     gnd: 8,
     vcc: 9,
     signal: 10,
+  },
+];
+
+let votes = [
+  {
+    id: 1,
+    label: "A",
+    count: 0,
+  },
+  {
+    id: 2,
+    label: "B",
+    count: 0,
+  },
+  {
+    id: 3,
+    label: "C",
+    count: 0,
   },
 ];
 
@@ -43,50 +61,86 @@ const resetVote = async (deviceId) => {
   );
 };
 
-const updateVotes = async () => {
+const updateVotes = async (chart) => {
   await Promise.all(
     seonsors.map(async (sensor) => {
       const res = await fetchVoteCount(sensor.id);
       const count = res?.count ?? 0;
-      const vote = document.getElementById(`vote-${sensor.id}`);
-      vote.innerText = String(count);
+      const vote = votes.find((vote) => vote.id === sensor.id);
+      const otherVotes = votes.filter((vote) => vote.id !== sensor.id);
+      votes = [...otherVotes, { ...vote, count }];
     })
   );
-};
-
-// device
-const obniz = new Obniz(obnizId);
-obniz.onconnect = async (obn) => {
-  console.info("device connected");
-  for (const sensor of seonsors) {
-    let start = Date.now();
-    const gnd = obn.getIO(sensor.gnd);
-    const vcc = obn.getIO(sensor.vcc);
-    const signal = obn.getIO(sensor.signal);
-    gnd.pull("5v");
-    gnd.drive("open-drain");
-    gnd.output(false);
-    vcc.output(true);
-    signal.input(async (value) => {
-      if (!value && Date.now() - start > 500) {
-        start = Date.now();
-        await sendVote(sensor.id);
-        console.info(`send data device id: ${sensor.id}`);
-        await updateVotes();
-      }
-    });
-  }
+  chart.data.datasets.forEach((dataset) => {
+    dataset.data = [votes.find((dress) => dress.label === dataset.label).count];
+  });
+  chart.data.datasets = chart.data.datasets.sort(
+    (a, b) => b.data[0] - a.data[0]
+  );
+  chart.update();
 };
 
 window.onload = () => {
-  updateVotes();
-  const resetButton = document.getElementById('reset');
-  resetButton.addEventListener('click', async () => {
-    await Promise.all(
-      seonsors.map(async (sensor) => {
-        await resetVote(sensor.id);
-      })
-    );
-    await updateVotes();
+  // chart
+  const ctx = document.getElementById("vote-chart");
+  const chart = new Chart(ctx, {
+    type: "bar",
+    data: {
+      labels: ["投票結果"],
+      datasets: votes.map(({ label, count }) => ({
+        label,
+        data: [count],
+      })),
+    },
+    options: {
+      scales: {
+        y: {
+          beginAtZero: true,
+          ticks: {
+            stepSize: 1,
+          },
+        },
+      },
+    },
   });
-}
+  updateVotes(chart);
+
+  // reset button
+  const searchParams = new URLSearchParams(window.location.search);
+  if (searchParams.get("role") === "admin") {
+    const resetButton = document.getElementById("reset");
+    resetButton.classList.remove("hidden");
+    resetButton.addEventListener("click", async () => {
+      await Promise.all(
+        seonsors.map(async (sensor) => {
+          await resetVote(sensor.id);
+        })
+      );
+      await updateVotes(chart);
+    });
+  }
+
+  // device
+  const obniz = new Obniz(obnizId);
+  obniz.onconnect = async (obn) => {
+    console.info("device connected");
+    for (const sensor of seonsors) {
+      let start = Date.now();
+      const gnd = obn.getIO(sensor.gnd);
+      const vcc = obn.getIO(sensor.vcc);
+      const signal = obn.getIO(sensor.signal);
+      gnd.pull("5v");
+      gnd.drive("open-drain");
+      gnd.output(false);
+      vcc.output(true);
+      signal.input(async (value) => {
+        if (!value && Date.now() - start > 500) {
+          start = Date.now();
+          await sendVote(sensor.id);
+          console.info(`send data device id: ${sensor.id}`);
+          await updateVotes(chart);
+        }
+      });
+    }
+  };
+};

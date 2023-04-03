07d643d7dfaa926647cfa1020b05948405a21bb3
ufoo68 <mitarou.yuutarou@gmail.com]>
2023-04-03 22:05:43 +0900

    誓いの言葉を反映

--- ui/index.js
+++ ui/index.js
@@ -23,17 +23,17 @@ const seonsors = [
 let votes = [
   {
     id: 1,
-    label: "A",
+    label: "1",
     count: 0,
   },
   {
     id: 2,
-    label: "B",
+    label: "2",
     count: 0,
   },
   {
     id: 3,
-    label: "C",
+    label: "3",
     count: 0,
   },
 ];
--- ui/result.js
+++ ui/result.js
@@ -1,7 +1,7 @@
 const voteDict = {
-  1: "A",
-  2: "B",
-  3: "C",
+  1: "病めるときも<br>健やかなるときも<br>ガチャ爆死したときも<br>手料理がまずかったときも<br>お互いを愛し尊重し許し合うことを誓います",
+  2: "1.明るく笑いで溢れる家庭を築きます<br>2.相手を思いやり助け合うことを誓います<br>3.お互いの家族や友人を 大切にしあいます<br>4.ノルちゃんを取られても恨まないようにします",
+  3: "「ゆ」友人や家族を大切にし<br>「う」嬉しいことも苦しいことも共に分かち合って<br>「た」沢山の思い出を一緒に作っていきます<br>「り」理解する気持ちを大切にし<br>「よ」喜びは2倍、悲しみは1/2で消化することを目標に<br>「う」うっかり失敗しても笑って許して<br>「こ」これからは二人で支えあって生きていくことを誓います",
 };
 
 const fetchVoteCount = async (voteId) => {
@@ -13,6 +13,9 @@ const fetchVoteCount = async (voteId) => {
 };
 
 const getRating = (votes, i) => {
+  if (i === 0) {
+    return 1;
+  }
   const previousVote = votes[i - 1];
   const currentVote = votes[i];
   return previousVote.count !== currentVote.count
@@ -35,10 +38,8 @@ window.onload = async () => {
   });
   for (const rank of ranks) {
     const rankLabel = document.getElementById(`rank-${rank.order}-label`);
-    rankLabel.innerText = rank.label;
-    const rankCount = document.getElementById(`rank-${rank.order}-count`);
-    rankCount.innerText = `${rank.count}票`;
+    rankLabel.innerHTML = rank.label;
     const rankRating = document.getElementById(`rank-${rank.order}-rating`);
-    rankRating.innerText = `${rank.rating}位`;
+    rankRating.innerText = `${rank.rating}位（${rank.count}票）`;
   }
 };
